e73a4914305b2fc0f3f9007739d4269e244a2c45
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-05-30 18:33:23 +0200

    Debut choix pions

--- src/main/java/fr/umontpellier/iut/rails/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/rails/vues/VueDuJeu.java
@@ -2,20 +2,16 @@ package fr.umontpellier.iut.rails.vues;
 
 import fr.umontpellier.iut.rails.IDestination;
 import fr.umontpellier.iut.rails.IJeu;
-import fr.umontpellier.iut.rails.mecanique.data.Destination;
 import javafx.collections.ListChangeListener;
 import javafx.event.EventHandler;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.effect.Effect;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-
-import java.util.List;
+import javafx.scene.text.Font;
 
 /**
  * Cette classe correspond à la fenêtre principale de l'application.
@@ -33,8 +29,12 @@ public class VueDuJeu extends HBox {
     private Button passer;
     private Label instruction;
     private VBox destinationsInitiales;
-    private VBox choixPionsInitiales;
+    private TextField choixPionsInitiales;
     private VueJoueurCourant vueJoueurCourant;
+    private VBox menuJoueur;
+    private HBox menuJeu;
+
+    private VBox boxJeu;
 
     public VueDuJeu(IJeu jeu) {
         this.jeu = jeu;
@@ -46,37 +46,35 @@ public class VueDuJeu extends HBox {
         instruction.setWrapText(true);
         destinationsInitiales = new VBox();
         destinationsInitiales.setStyle("-fx-alignment: center;-fx-padding: 0 0 25 0;-fx-spacing: 5");
-        choixPionsInitiales = new VBox();
-        Label choixPionsInitialesTitre = new Label("Entrez le nombre de wagon que vous voulez prendre (Minimum X, maximum Y) :");
-        choixPionsInitialesTitre.setStyle("-fx-text-fill: white");
-        TextField choixPionsInitialesField = new TextField();
-        choixPionsInitiales.getChildren().addAll(
-                choixPionsInitialesTitre,
-                choixPionsInitialesField
-        );
+        choixPionsInitiales = new TextField();
+        choixPionsInitiales.setMaxWidth(50);
+        choixPionsInitiales.setBorder(Border.EMPTY);
+        choixPionsInitiales.setFont(new Font(30));
         choixPionsInitiales.setStyle("-fx-spacing: 5;-fx-padding: 0 0 25 0;");
         vueJoueurCourant = new VueJoueurCourant();
         vueJoueurCourant.setStyle("-fx-padding: 0 0 15 0");
         Label titre = new Label("Aventuriers du Rail");
         titre.setStyle("-fx-padding: 25 0 25 0; -fx-text-alignment: center; -fx-alignment: center; -fx-font-size: 17; -fx-text-fill: white");
-        VBox menu = new VBox();
-        menu.getChildren().addAll(
+        menuJoueur = new VBox();
+        //menuJoueur.setStyle("-fx-spacing: 200");
+        menuJoueur.getChildren().addAll(
                 titre,
                 vueJoueurCourant,
                 instruction,
-                choixPionsInitiales,
+                //choixPionsInitiales,
                 destinationsInitiales,
-                passer);
-        menu.setMinWidth(200);
-        menu.setStyle("-fx-border-width: 5; -fx-border-color: linear-gradient(#0014bd, #4d00bd); -fx-padding: 5; -fx-background-color: linear-gradient(#007693, #4d00bd)");
-        menu.setAlignment(Pos.TOP_CENTER);
-        VBox logs = new VBox();
-        Label logStartDisplay = new Label("Logs de jeu :");
-        logStartDisplay.setStyle("-fx-text-fill: white; -fx-font-size: 20");
+                passer
+        );
+        menuJoueur.setMinWidth(200);
+        menuJoueur.setStyle("-fx-border-width: 5; -fx-border-color: linear-gradient(#0014bd, #4d00bd); -fx-padding: 5; -fx-background-color: linear-gradient(#007693, #4d00bd)");
+        menuJoueur.setAlignment(Pos.TOP_CENTER);
+        /*VBox logs = new VBox();
+        Label logTitle = new Label("Logs du jeu :");
+        logTitle.setStyle("-fx-text-fill: white; -fx-font-size: 20");
         ScrollPane logsBox = new ScrollPane();
         logs.setStyle("-fx-background-color: linear-gradient(to right, #005063, #4d00bd); -fx-spacing: 2;-fx-padding: 5");
-        logs.setMinHeight(224);
-        logs.setMinWidth(1336);
+        logs.setMinHeight(118);
+        logs.setMinWidth(1320);
         logsBox.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         logsBox.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         logsBox.setContent(logs);
@@ -87,8 +85,24 @@ public class VueDuJeu extends HBox {
                 plateau,
                 logsBox
         );
-        logs.getChildren().add(logStartDisplay);
-        getChildren().addAll(PlateauEtLog,menu);
+        logs.getChildren().add(logTitle);
+        String[] Lorem = new String[]{"lorem","ipsum","dolor","sit","amet","consectetur","adipiscing","elit","vitae","blandit","libero","luctus","dapibus","est","nullam","vel","mattis"};
+        for (int i=0;i<20;i++){
+            Label logLoremIpsum = new Label("[INFO] - ");
+            for (int j=0;j<20;j++){
+                logLoremIpsum.setText(logLoremIpsum.getText()+" "+Lorem[(int)(Math.random()*(Lorem.length))]);
+            }
+            logLoremIpsum.setText(logLoremIpsum.getText()+".");
+            logLoremIpsum.setStyle("-fx-text-fill: white");
+            logs.getChildren().add(logLoremIpsum);
+        }PlateauEtLog*/
+        VBox boxJeu = new VBox();
+        menuJeu = new HBox();
+        boxJeu.getChildren().addAll(
+                plateau,
+                menuJeu
+        );
+        getChildren().addAll(boxJeu, menuJoueur);
     }
 
     final ListChangeListener<IDestination> destinationsInitialesChanges = change -> {
@@ -113,6 +127,10 @@ public class VueDuJeu extends HBox {
                         }
                     }
                 }
+                if (destinationsInitiales.getChildren().size() == 0){
+                    menuJoueur.getChildren().remove(destinationsInitiales);
+                    menuJoueur.getChildren().add(3,choixPionsInitiales);
+                }
             }
         }
     };
@@ -131,6 +149,13 @@ public class VueDuJeu extends HBox {
         return jeu;
     }
 
-    EventHandler<? super MouseEvent> actionPasserParDefaut = (mouseEvent -> getJeu().passerAEteChoisi());
+    EventHandler<? super MouseEvent> actionPasserParDefaut = (mouseEvent -> {
+        if (menuJoueur.getChildren().contains(choixPionsInitiales)){
+            getJeu().leNombreDePionsSouhaiteAEteRenseigne(choixPionsInitiales.getText());
+        } else { // TODO Loan doit finir ça, pour choisir les pions
+            getJeu().passerAEteChoisi();
+        }
+
+    });
 
 }
