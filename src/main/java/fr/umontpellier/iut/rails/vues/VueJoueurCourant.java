98c47bd401e1792bb7a6d1b0330c034443c5f84d
LABORIE Quentin <quentin.laborie@etu.umontpellier.fr>
2023-06-06 15:26:35 +0200

    VueDuJeu : avatars des joueurs fini

--- src/main/java/fr/umontpellier/iut/rails/vues/VueJoueurCourant.java
+++ src/main/java/fr/umontpellier/iut/rails/vues/VueJoueurCourant.java
@@ -3,21 +3,15 @@ package fr.umontpellier.iut.rails.vues;
 import fr.umontpellier.iut.rails.ICarteTransport;
 import fr.umontpellier.iut.rails.IDestination;
 import fr.umontpellier.iut.rails.IJoueur;
-import fr.umontpellier.iut.rails.mecanique.data.CarteTransport;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 /**
@@ -30,29 +24,43 @@ public class VueJoueurCourant extends VBox { // TODO Quentin afficher les avatar
     private Label nomJoueur;
     private VBox carteTransport;
     private VBox carteDestination;
-//    private ImageView avatar;
+    private ImageView avatar;
     private ScrollPane sp;
 
     public VueJoueurCourant() {
         nomJoueur = new Label();
+
         carteTransport = new VBox();
+
         carteDestination = new VBox();
-//        avatar = new ImageView();
+
+        avatar = new ImageView();
+        avatar.setFitHeight(120.0);
+        avatar.setFitWidth(94.86);
+
         sp = new ScrollPane();
         sp.setContent(carteTransport);
         sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         sp.setPrefViewportHeight(350.0);
-        getChildren().addAll(/*avatar,*/nomJoueur,sp,carteDestination);
         sp.setManaged(false);
         sp.setVisible(false);
+
+        getChildren().addAll(avatar,nomJoueur,sp,carteDestination);
+        this.setSpacing(10.0);
+        this.setAlignment(Pos.CENTER);
     }
 
     ChangeListener<IJoueur> JoueurCourantChange = (observableValue, ancien, courant) -> {
-        String couleurAvatar = new String();
+        String couleurAvatar = "avatar-" + courant.getCouleur() + ".png";
+        Image portrait = new Image("images/cartesWagons/" + couleurAvatar);
+        avatar.setImage(portrait);
+
         String carte = new String();
+
         nomJoueur.setText(courant.getNom());
         nomJoueur.setStyle("-fx-text-fill: white");
         carteTransport.getChildren().clear();
+
         for(ICarteTransport ct : courant.getCartesTransport()) {
             if (ct.estWagon()) {
                 carte = "carte-WAGON-";
@@ -82,14 +90,14 @@ public class VueJoueurCourant extends VBox { // TODO Quentin afficher les avatar
             sp.setManaged(true);
             sp.setVisible(true);
         }
+
         carteDestination.getChildren().clear();
         for (IDestination cd : courant.getDestinations()){
             Label labelCarteDestination = new Label(cd.getVilles().toString());
             labelCarteDestination.setStyle("-fx-text-fill: white");
             carteDestination.getChildren().add(labelCarteDestination);
         }
-//        couleurAvatar = "avatar-" + courant.getCouleur();
-//        avatar.setId("images/cartesWagons/avatar-BLEU.png");
+
     };
 
     public void creerBindings() {
